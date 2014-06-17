0414fcf769e7847849adcc7c891f9410a2c09083
Joshua M. Clulow <jmc@joyent.com>
2014-06-17 18:57:10 +0000

    add RunQueue for execution coordination

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /* vim: set ts=8 sts=8 sw=8 noet: */
 
 var lib_ur_client = require('./lib/ur_client');
+var lib_runqueue = require('./lib/runqueue');
 
 function
 create_ur_client(options)
@@ -10,6 +11,15 @@ create_ur_client(options)
 	return (ur);
 }
 
+function
+create_run_queue(options)
+{
+	var rq = new lib_runqueue.RunQueue(options);
+
+	return (rq);
+}
+
 module.exports = {
-	create_ur_client: create_ur_client
+	create_ur_client: create_ur_client,
+	create_run_queue: create_run_queue
 };
